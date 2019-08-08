7c841adcb5c0e9df5676feb8631f66f96df49da5
‘Tim <timothy.j.ocampo@gmail.com>
2019-08-08 12:10:54 -0700

    added reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,30 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+
+    var revStr1 = "";
+    var revStr2 ="";
+
+    //or:
+    let myArr = str.split('').reverse();
+    let myResultArr = myArr.join(''); //remember to put the chars back into a string!
+
+    for(var i=str.length-1; i>=0; i--){
+        revStr1 = revStr1+str[i]
+    }
+
+    //or:
+    for(let char of str){
+        revStr2 = char+revStr2 //Notice char came before revStr2
+    }
+
+    //or: (SuperDuper)          //1st arg is accumulated item to bring back   //2nd arg is each char that get accumulated
+    let superDuperSolution = str.split('').reduce((revString , char)=> char + revString, '');
+
+    return superDuperSolution
+}
 
 module.exports = reverse;
+
+
