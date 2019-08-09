b60f61fa3b97d2620dbcfc51dd30b8ef3656819f
‘Tim <timothy.j.ocampo@gmail.com>
2019-08-09 14:09:01 -0700

    added palindrome : splt('').reverse('').join('') approach

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,15 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+
+    const rev = str.split('').reverse().join('');
+    return rev === str
+
+
+}
+
 
 module.exports = palindrome;
+
+
